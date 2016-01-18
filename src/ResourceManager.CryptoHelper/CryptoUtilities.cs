e23d35a68b8c70286d828e3f50d900ce075eb003
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-01-18 16:47:18 +0100

    Added cryptohelper

---
+++ src/ResourceManager.CryptoHelper/CryptoUtilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.Azure.ResourceManager.CryptoHelper
+{
+    public class CryptoUtilities
+    {
+        public static IEnumerable<string> GetKeys(int length = 32, int count = 2)
+        {
+            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+            {
+                var key = new byte[length];
+                foreach (var i in Enumerable.Range(0, count))
+                {
+                    rngCsp.GetBytes(key);
+                    yield return Convert.ToBase64String(key);
+                }
+            }
+        }
+    }
+}
---
+++ test/S-Innovations.Azure.ResourceManager.Tests/CryptoTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SInnovations.Azure.ResourceManager.Tests
+{
+    [TestClass]
+    public class CryptoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var keys = CryptoHelper.CryptoUtilities.GetKeys().ToArray();
+            Assert.AreNotEqual(keys[0], keys[1]);
+        }
+    }
+}
